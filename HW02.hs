a43bb529cf9f4c74a4e58372f7f8e350c977ba87
thanhdo89se <thanhdo89se@gmail.com>
2017-04-07 18:06:00 +0700

    Add HW02 ext1-2-3-4

--- HW02.hs
+++ HW02.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW02 where
+import Data.List
 
 -- Mastermind -----------------------------------------
 
@@ -23,28 +24,34 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches [] _ = 0
+exactMatches (_:_) [] = 0
+exactMatches xs ys = length . filter (==True) $ zipWith (==) xs ys
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors code = [length $ filter (==c) code | c <- colors]
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches xs ys = length $ intersect xs ys
+-- matches xs ys = sum $ zipWith min (countColors xs) (countColors ys)
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove x y = Move y m (n - m)
+    where m = exactMatches x y
+          n = matches x y
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move xs m n) ys = m == m' && n == n'
+    where Move _ m' n' = getMove xs ys
 
 -- Exercise 5 -----------------------------------------
 
@@ -65,3 +72,5 @@ solve = undefined
 
 fiveGuess :: Code -> [Move]
 fiveGuess = undefined
+
+-- Thanks to https://gist.github.com/adolfopa/2cb09306d4c287719601
