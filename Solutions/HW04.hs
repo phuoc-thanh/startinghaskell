e3f183bdb2be4efb2da975d0f84ec595f6737f26
thanhdo89se <thanhdo89se@gmail.com>
2017-05-10 17:21:30 +0700

    Rework exercise 03,04 HW04

--- Solutions/HW04.hs
+++ Solutions/HW04.hs
@@ -18,21 +18,38 @@ instance (Num a, Eq a) => Eq (Poly a) where
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show (P f) = showP $ dropWhileEnd (==0) (reverse f)
+    show (P f) = concat . intersperse " + " . reverse $ toTerms f
 
-showP :: (Num a, Eq a, Show a) => [a] -> String
-showP [] = ""
-showP (z:zs)
-    | zs == [] = show z
-    | z == 0 = showP zs
-    | z == 1 = "x" ++ "^" ++ show (length zs) ++ " + " ++ showP zs
-    | length zs == 1 = show z ++ "x" ++ " + " ++ showP zs
-    | otherwise = show z ++ "x" ++ "^" ++ show (length zs) ++ " + " ++ showP zs
+toTerms :: (Num a, Eq a, Show a) => [a]-> [String]
+toTerms a = filter (\t -> t /= "0" && (not . null $ t)) . map toTerm $ zipWithIndex a
+
+zipWithIndex :: [a] -> [(Int, a)]
+zipWithIndex = zip [0,1..]
+
+toTerm :: (Num a, Eq a, Show a) => (Int, a) -> String
+toTerm (0, c)  =  show c
+toTerm (_, 0)  = ""
+toTerm (1, 1)  = "x"
+toTerm (1, -1) = "-x"
+toTerm (1, c)  = show c ++ "x"
+toTerm (d, 1)  = "x" ++ "^" ++ show d
+toTerm (d, -1) = "-x" ++ "^" ++ show d
+toTerm (d, c)  = show c ++ "x" ++ "^" ++ show d
 
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus (P xs) (P ys) = P (zipWith (+) xs ys)
+plus (P a) (P b) = P (rewrite a b)
+
+rewrite ::Num a => [a] -> [a] -> [a]
+-- rewrite i xs
+--     | i > length xs = rewrite i (xs ++ [0])
+--     | otherwise = xs
+rewrite ys zs
+    | length ys == length zs = zipWith (+) ys zs
+    | length ys > length zs = rewrite ys (zs ++ [0])
+    | otherwise = rewrite (ys ++ [0]) zs
+
 
 -- Exercise 5 -----------------------------------------
 
@@ -67,3 +84,4 @@ class Num a => Differentiable a where
 instance Num a => Differentiable (Poly a) where
     deriv = undefined
 
+--thanks to https://github.com/amar47shah/cis-194/tree/master/2015-noamz
