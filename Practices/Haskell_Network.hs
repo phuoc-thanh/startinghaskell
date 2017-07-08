b58cd888961d5b54daa1150b53d729da5c2e5739
thanhdo89se <thanhdo89se@gmail.com>
2017-07-08 23:05:04 +0700

    Add some practice on HTTP and Network package

--- Practices/Haskell_Network.hs
+++ Practices/Haskell_Network.hs
@@ -1,13 +1,53 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+
 import Network
+import Network.URI
+-- import Network.HTTP
+import Network.Browser
 import System.IO
+
+import           Data.Aeson            (Value)
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Yaml             as Yaml
+import           Network.HTTP.Simple
  
-main = withSocketsDo $ do
-    h <- connectTo "danaphalife.com" (PortNumber 80)
+sampleReq = withSocketsDo $ do
+    h <- connectTo "api.kimdungqq.com" (PortNumber 80)
     hSetBuffering h LineBuffering
-    hPutStr h "GET /vi HTTP/1.1\nhost: danaphalife.com\n\n"
+    hPutStr h "GET /jinyong/api/client_api/defaultSid.php?acc=11111&sid=1&opname=10&sign=8270b3084d30ab3d95a7d58dbb15ed73 HTTP/1.1\nhost: api.kimdungqq.com\n\n"
     contents <- hGetContents h
     putStrLn contents
     hClose h
 
 --https://wiki.haskell.org/Cookbook/Network_programming
+
+
+Just keepAliveURI = parseURI "http://api.kimdungqq.com/jinyong/vega/loginVerify"
+body = "uid=11111&token=6892aea2-d3da-48d8-84c6-fcc4782f3d21&os=and&version=75896"
+-- body = "partnerId=0&userName=reply1988&deviceId=d80f998b16396187&serverMode=UNKNOWN&password=haxinhdep&refcode=0&gameId=46"
+-- headers = [Header HdrContentType "application/x-www-form-urlencoded",
+--            Header HdrContentLength (show . length $ body)]
+
+-- keepAliveRq = Request  {
+--     rqURI = keepAliveURI, rqMethod = POST,
+--     rqHeaders = headers,
+--     rqBody = body
+-- }
+
+-- keepAliveRq = setRequestPath "/payclient.ashx?op=CheckUser"
+keepAliveRq = setRequestPath "/jinyong/vega/loginVerify"
+            $ setRequestHost "api.kimdungqq.com"
+            -- $ setRequestHost "m-pay.kimdungqq.com"
+            $ setRequestBodyLBS body
+            $ setRequestMethod "POST"
+            $ defaultRequest
+
+main = do
+    response <- httpLBS keepAliveRq
+    putStrLn $ "The Header: " ++
+               show (getResponseHeaders response)
+    putStrLn $ show $ response
+-- main = browse $ do
+--      res <- request keepAliveRq
+--      putStrLn show $ getResponseBody res
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # startinghaskell
 Learning Haskell with CIS-194 Spring 2015
+
+## Haskell Platform Install
+
+1. Download the minimum package at: https://www.haskell.org/platform/windows.html
+
+2. Install Haskell platform and VS Code
+
+3. Install ghc-mod: cabal install ghc-mod
+
+4. Install Haskell Syntax Highlighting, Haskell ghc-mod Extension (search on VS Code)
+
+5. Network package: network, HTTP, http-conduit, yaml
