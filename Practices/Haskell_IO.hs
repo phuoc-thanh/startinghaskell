076d21c77627d67ddb8ed4f9624d5e714aaaac19
thanhdo89se <thanhdo89se@gmail.com>
2018-01-02 17:47:47 +0700

    add a test for hashtable

--- Practices/Haskell_IO.hs
+++ Practices/Haskell_IO.hs
@@ -2,9 +2,12 @@
 module Haskell_IO (main) where
 
 import qualified Data.ByteString as BS
+import qualified Data.HashTable.IO as H
 import Data.ByteString (ByteString)
 import Data.Functor
+import Data.Maybe
 import Data.Word8
+import Control.Monad
 
 
 main :: IO ()
@@ -44,3 +47,13 @@ getWords = BS.split 32 <$> BS.getLine
 --   ln <- BS.getLine
 --   return $ BS.split 32 ln -- 32 is the ASCII code for ' '
 
+
+type HashTable k v = H.LinearHashTable k v
+
+-- foo :: IO (HashTable Int Int)
+foo = do
+    ht <- H.new :: IO (HashTable Int Int)
+    forM_ [1..1000000] $ \x -> H.insert ht x x
+    n <- H.lookup ht 79909
+    print n
+    -- return ht
