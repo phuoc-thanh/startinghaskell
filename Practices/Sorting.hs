cb894a6ad7bbd334772c667f05f6b2ba198d906f
Thanh Do <thanhdo89se@gmail.com>
2019-06-17 18:11:05 +0700

    add radix sort

--- Practices/Sorting.hs
+++ Practices/Sorting.hs
@@ -3,6 +3,22 @@
 module Sorting where
 
 
+-- In-efficient sorts will be skipped (selection, insertion...)
+-- Only efficient sorts and not-imperative sorts are implemented:
+-- Bubble Sort | Merge Sort | Quick Sort | Radix Sort
+
+arr = [170, 45, 75, 90, 02, 802, 2, 66]
+
+-- Maximum of a list, can be done with one bubble (swap method) sort
+-- Or with default Prelude func
+-- max_ xs = maximum
+max_ (x:[])     = x
+max_ (x:y:xs)
+    | x > y     = max_ (x:xs)
+    | otherwise = max_ (y:xs)
+
+-- Comparison-Based Sort Algorithms, include Bubble Sort, Merge Sort, Quick Sort
+
 -- | Bubble Sort
 -- Time Complexity : O(n^2) in theory, but this implementation is: n + (n-1) + .. + 1.
 -- Time Complexity : T = n*(n+1) / 2
@@ -27,7 +43,7 @@ merge_sort xs  = merge (merge_sort (fst m))  (merge_sort (snd m)) where
     merge x [] = x
     merge [] y = y
     merge x y
-        | head x < head y = head x : (merge (tail x) y)
+        | head x > head y = head x : (merge (tail x) y)
         | otherwise       = head y : (merge x (tail y))
 
 -- | Quick Sort
@@ -38,3 +54,9 @@ quick_sort [] = []
 quick_sort (x:xs) = (quick_sort left) ++ [x] ++ (quick_sort right) where
     left  = filter (>= x) xs
     right = filter (<  x) xs
+
+-- Non Comparison-based
+radix_sort (x:xs) = map (\x -> mod x 10) (x:xs) 
+
+-- n times power of 10
+radix n xs  = filter (\x -> div x (10^n) == 0) xs
